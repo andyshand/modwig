0520103342e0de5deb8e85fb4e4f319125451cc2
Andy Shand <andrewshand94@gmail.com>
2020-09-24 18:19:43 +0100

    Add setup steps

--- src/renderer/setup/Setup.tsx
+++ src/renderer/setup/Setup.tsx
@@ -7,15 +7,23 @@ const Button = styled.div`
     padding: .5em 2em;
     margin: 0 auto;
     display: inline-block;
+    cursor: pointer;   
+    &:hover {
+        filter: brightness(85%);
+    }
+    border-radius: .2em;
+    margin-top: 1.6rem;
 `
 const Video = styled.video`
-
     width: 80%;
     margin: 0 auto;
+    margin-bottom: 1.6rem;
     display: block;
 `
 const StepText = styled.div`
     text-align: center;
+    max-width: 25em;
+    margin: 0 auto;
 `
 const StepWrap = styled.div`
     display: flex;
@@ -27,20 +35,31 @@ const StepWrap = styled.div`
 `
 const StepCircles = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem 0;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
 `
 const StepCircle = styled.div`
-    width: 2em;
-    height: 2em;
-    background: #CCC;
+    width: .7em;
+    height: .7em;
+    background: ${(props: any) => props.isActive ? `#CCC` : `#666`};
     border-radius: 100%;
+    cursor: pointer;
+    &:hover {
+        background: #999;
+    }
 
     &:not(:last-child) {
-        margin-right: 1em;
+        margin-right: .5em;
     }
 `
 export class Setup extends React.Component {
     state = {
-        step: 0
+        step: 3
     }
     step0() {
         return {
@@ -51,10 +70,28 @@ export class Setup extends React.Component {
     }
     step1() {
         return {
-            description: 'Open Bitwig Settings and enable the "Bitwig Enhancement Suite" controller in Bitwig.',
-            content: <Video autoPlay>
+            description: 'Open Bitwig Settings and enable the "Bitwig Enhancement Suite" controller.',
+            content: <Video loop autoPlay>
                 <source src={getResourcePath('/videos/setup-0.mp4')} type="video/mp4" />
             </Video>
+        }
+    }
+    step2() {
+        return {
+            description: <div>
+                Bitwig needs accessibility access in order to monitor keyboard shortcuts globally.
+                <Button>Enable Accessibility Access</Button>
+            </div>,
+            content: null
+        }
+    }
+    step3() {
+        return {
+            description: <div>
+                All done! If you ever need to see these steps again, click the icon in the menu bar and choose "Setup..."
+            </div>,
+            canContinue: true,
+            content: null
         }
     }
     getStepCount() {
@@ -78,6 +115,7 @@ export class Setup extends React.Component {
             this.setState({step: nextI}) 
         } else {
             // we're done!
+            window.location.href = '#/settings'
         }
     }
     setStep = (i) => {
@@ -89,16 +127,15 @@ export class Setup extends React.Component {
         const currStep = this['step' + this.state.step]()
         return <StepWrap>
             <div>
-                <div>Step {this.state.step + 1}</div>
                 {currStep.content}
+                <StepCircles>
+                    {this.times(this.getStepCount()).map(i => {
+                        return <StepCircle isActive={i === this.state.step} onClick={() => this.setStep(i)} />
+                    })}
+                </StepCircles>
                 <StepText>
-                    <StepCircles>
-                        {this.times(this.getStepCount()).map(i => {
-                            return <StepCircle onClick={() => this.setStep(i)} />
-                        })}
-                    </StepCircles>
                 <div>{currStep.description}</div>
-                {currStep.canContinue ? <Button onClick={this.onNextStep}>Continue</Button> : null}
+                {currStep.canContinue ? <Button onClick={this.onNextStep}>{this.state.step === this.getStepCount() - 1 ? `Finish and Customise Settings` : `Continue`}</Button> : null}
                 </StepText>
             </div>
         </StepWrap>
